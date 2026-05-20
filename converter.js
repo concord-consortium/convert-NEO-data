16c54ab195746e7976e615b93ec46acd8e823cd3
Dan Damelin <ddamelin@concord.org>
2026-05-20 18:26:52 -0400

    feat: extract MM/DD/YYYY date from NEO filenames

--- converter.js
+++ converter.js
@@ -1,7 +1,22 @@
 (function (root) {
   "use strict";
 
-  var api = {};
+  function extractIsoDate(filename) {
+    var m = String(filename).match(/(\d{4})-(\d{2})-(\d{2})/);
+    return m ? { year: m[1], month: m[2], day: m[3] } : null;
+  }
+
+  function parseDateFromFilename(filename) {
+    var d = extractIsoDate(filename);
+    if (!d) {
+      throw new Error("No date (YYYY-MM-DD) found in filename: " + filename);
+    }
+    return d.month + "/" + d.day + "/" + d.year;
+  }
+
+  var api = {
+    parseDateFromFilename: parseDateFromFilename
+  };
 
   if (typeof module !== "undefined" && module.exports) {
     module.exports = api;
--- tests/converter.test.js
+++ tests/converter.test.js
@@ -9,3 +9,21 @@ const c = require("../converter.js");
 test("converter module exports an object", () => {
   assert.strictEqual(typeof c, "object");
 });
+
+test("parseDateFromFilename converts a .csv filename to MM/DD/YYYY", () => {
+  assert.strictEqual(
+    c.parseDateFromFilename("MYD28M_2025-05-01_rgb_360x180.csv"),
+    "05/01/2025"
+  );
+});
+
+test("parseDateFromFilename handles the .SS.CSV extension", () => {
+  assert.strictEqual(
+    c.parseDateFromFilename("MYD28M_2025-04-01_rgb_360x180.SS.CSV"),
+    "04/01/2025"
+  );
+});
+
+test("parseDateFromFilename throws when no date is present", () => {
+  assert.throws(() => c.parseDateFromFilename("no_date_here.csv"), /No date/);
+});
